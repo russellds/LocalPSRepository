ef14b7e551154b457658ab60b70c9ae14569e94a
Russell Slater <russellds@gmail.com>
2017-08-30 23:38:20 -0400

    completely removed -force from start-build !deploy

--- build/Start-Build.ps1
+++ build/Start-Build.ps1
@@ -12,7 +12,7 @@ if(-not (Get-Module -ListAvailable PSDepend))
 Import-Module PSDepend
 $null = Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
 
-Set-BuildEnvironment -Force
+Set-BuildEnvironment
 
 Invoke-psake $PSScriptRoot\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
